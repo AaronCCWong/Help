dbb74fecd94fddacf0a8bc096229aba3c0f141e1
AaronCCWong <aarn.wong@gmail.com>
2015-09-15 13:56:50 -0400

    Add assocations between reviews and restaurants in backbone.

--- app/assets/javascripts/models/restaurant.js
+++ app/assets/javascripts/models/restaurant.js
@@ -1,3 +1,21 @@
 YelpClone.Models.Restaurant = Backbone.Model.extend({
-  urlRoot: '/api/restaurants'
+  urlRoot: '/api/restaurants',
+
+  parse: function(response) {
+    if (response.reviews) {
+      this.reviews().set(response.reviews);
+      delete(response.reviews);
+    }
+    return response;
+  },
+
+  reviews: function() {
+    if (!this._reviews) {
+      this._reviews = new YelpClone.Collections.Reviews([], {
+        restaurant: this
+      });
+    }
+
+    return this._reviews;
+  }
 });
