4b22df105f58c61289d02f94c3000acc8f80db80
Aaron Wong <aarn.wong@gmail.com>
2015-09-21 22:06:09 -0400

    Add some more seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,3 +101,17 @@ Review.create(
   body: 'After I eat here, I fall asleep while pair programming. You have to try the firebird!',
   rating: 5
 )
+
+Review.create(
+  user_id: 8,
+  restaurant_id: 1,
+  body: 'If I didnt smell like curry all day that day, the day after, and even possibly the day after that, this place would get five stars. Stopped by here right after stopping by the uncommons as a quick bite and it was worth it! The food:  Im pretty snobby when it comes to East Asian curry. Even more so when it comes to donkatsu. But I was definitely satisfied here! For five or six dollars, this is some of the best food you can get. For a little extra, you can add a sausage, an egg, some deep fried shrimp, etc. Its great. The curry was slightly spicy but the hot sauce on premise makes it just right. The donkatsu was fried well and the rice was neither too little or too much in both quantify and overall doneness. The other add ons seemed interesting but Ill have to come again to try them out! The venue:  Small and cramped. It only has seating for maybe eight people at most. Definitely not a place to come sit and eat with a crowd or even when its rush hour. Come here late at night or just to grab something to-go. The service:  No real service here. Just order, get your order, eat, return tray. The guy was nice though if that counts. Overall would definitely come again to try their other options. Just maybe with a change of clothes wrapped in odorless bags.',
+  rating: 4
+)
+
+Review.create(
+  user_id: 5,
+  restaurant_id: 1,
+  body: 'This place rocks! If you want some delicious katsu curry without having to pay a bunch of money and tip to a waiter at some restaurant, this place is great! Beware, it reeks of fried food in there and you will too if you eat inside! Small place, you might as well go eat in Washington Square Park (a block away) anyways... Simple menu, good prices, service is usually (though not always) great! Check it out!',
+  rating: 5
+)
